430115116886654584df71f88397756a2ed35113
Hamii <hamop3108@gmail.com>
2023-07-22 10:47:46 +0300

    Task Five Created

---
+++ TheWebsiteGame/Controllers/FifthHomeController.cs
@@ -0,0 +1,39 @@
+﻿namespace TheWebsiteGame.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+    using TheWebsiteGame.Models;
+
+    public class FifthHomeController : Controller
+    {
+        [HttpGet]
+        public IActionResult TaskFive()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult TaskFive(TaskFiveViewModel model) 
+        {
+            if (model.AnswerGiven.ToLower() != TaskFiveViewModel.AnswerA.ToLower() && model.AnswerGiven.ToLower() != TaskFiveViewModel.AnswerB.ToLower())
+            {
+                return View(model);
+            }
+            else if (!this.ModelState.IsValid)
+            {
+                return View(model);
+            }
+            else
+            {
+                if (model.AnswerGiven.ToLower() == TaskFiveViewModel.AnswerA.ToLower())
+                {
+                    TaskFiveViewModel.Message = "Not yet";
+                }
+                if (model.AnswerGiven.ToLower() == TaskFiveViewModel.AnswerB.ToLower())
+                {
+                    TaskFiveViewModel.Message = "I don't like liars";
+                }
+                TaskFiveViewModel.TaskCompleted = true;
+                return RedirectToAction("Index", "Home");
+            }
+        }
+    }
+}
---
+++ TheWebsiteGame/Models/TaskFiveViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace TheWebsiteGame.Models
+{
+    public class TaskFiveViewModel
+    {
+        public static string AnswerA = "No";
+        public static string AnswerB = "Yes";
+        public static string Message = " ";
+        public string AnswerGiven { get; set; } = null!;
+        public static string ImageUrl = "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fpreview.redd.it%2Fhow-would-you-introduce-alternates-from-the-mandela-catalog-v0-hq4k33z5qm4a1.jpg%3Fauto%3Dwebp%26s%3D20843e083b2ba03e1d25d468fa7f165385777a09&f=1&nofb=1&ipt=0b04af4e29f120ceeb79bb0448159bcbd8e8277210c3adec7e8f4a214839718f&ipo=images";
+        public static bool TaskCompleted = false;
+    }
+}
